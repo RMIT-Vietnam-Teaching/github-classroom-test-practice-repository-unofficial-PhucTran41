1b3cf2865c8aac604207aef5f91b22c865e294eb
Phuc Tran <s3929597@rmit.edu.vn>
2025-08-24 12:51:51 +0000

    Complete question 2

---
+++ question-2/project_setup.sh
@@ -0,0 +1,9 @@
+mkdir my_app
+cd my_app
+mkdir src logs
+cd src
+touch app.js
+cd ../logs
+touch report.log
+echo "Project Initialized by: Tran Phan Hoang Phuc" >> report.log
+echo "Setup completed on: $(date)" >> report.log
